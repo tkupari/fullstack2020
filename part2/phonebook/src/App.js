4254f6231f8ff625fe410ddf2580a4f12b17c001
Teemu Kupari <tkupari@gmail.com>
2020-08-07 13:08:33 +0300

    2.8: Add numbers to phonebook

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -2,12 +2,16 @@ import React, { useState } from 'react'
 
 const App = () => {
   const [ persons, setPersons ] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas', number: '040-123456' }
   ]) 
   const [ newName, setNewName ] = useState('')
+  const [ newNumber, setNewNumber ] = useState('')
 
   const handleNameChange = (event) => {
     setNewName(event.target.value)
+  }
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value)
   }
 
   const addName = (event) => {
@@ -16,8 +20,9 @@ const App = () => {
       if(persons.map(p => p.name).includes(newName)) {
         alert(`${newName} is already added to phonebook`)
       } else {
-        setPersons(persons.concat({name: newName}))
+        setPersons(persons.concat({name: newName, number: newNumber}))
         setNewName('')
+        setNewNumber('')
       }
     }
   }
@@ -28,13 +33,14 @@ const App = () => {
       <form onSubmit={addName}>
         <div>
           name: <input value={newName} onChange={handleNameChange}/>
+          number: <input value={newNumber} onChange={handleNumberChange}/>
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map(person => <p>{person.name}</p>)}
+      {persons.map(person => <p>{person.name} {person.number}</p>)}
     </div>
   )
 }
