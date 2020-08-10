4088b700866abaac4ecc24b16a570ba7709054ee
Teemu Kupari <tkupari@gmail.com>
2020-08-10 15:05:09 +0300

    2.10: Phonebook split into components

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,15 +1,19 @@
 import React, { useState } from 'react'
 
-const App = () => {
-  const [ persons, setPersons ] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ]) 
+const Filter = ({ filter, setFilter }) => {
+  const handleFilterChange = (event) => {
+    setFilter(event.target.value)
+  }
+  return (
+      <div>
+        filter shown with <input value={filter} onChange={handleFilterChange}/>
+      </div>
+  )
+}
+
+const PersonForm = ({ persons, setPersons }) => {
   const [ newName, setNewName ] = useState('')
   const [ newNumber, setNewNumber ] = useState('')
-  const [ filter,  setFilter ] =  useState('')
 
   const handleNameChange = (event) => {
     setNewName(event.target.value)
@@ -17,15 +21,6 @@ const App = () => {
   const handleNumberChange = (event) => {
     setNewNumber(event.target.value)
   }
-  const handleFilterChange = (event) => {
-    setFilter(event.target.value)
-  }
-  const personsToShow = () => {
-    if(filter.length > 0)
-      return persons.filter(p => p.name.toLowerCase().includes(filter))
-    return persons
-  }
-
   const addName = (event) => {
     event.preventDefault()
     if(newName.length > 0) {
@@ -38,14 +33,7 @@ const App = () => {
       }
     }
   }
-
   return (
-    <div>
-      <h2>Phonebook</h2>
-      <div>
-        filter shown with <input value={filter} onChange={handleFilterChange}/>
-      </div>
-      <h2>add a new</h2>
       <form onSubmit={addName}>
         <div>
           name: <input value={newName} onChange={handleNameChange}/>
@@ -55,8 +43,38 @@ const App = () => {
           <button type="submit">add</button>
         </div>
       </form>
+  )
+}
+
+const Persons = ({ persons, filter }) => {
+  const personsToShow = filter.length > 0
+    ? persons.filter(p => p.name.toLowerCase().includes(filter))
+    : persons
+  return (
+    <div>
+    {personsToShow.map(person => <p>{person.name} {person.number}</p>)}
+    </div>
+  )
+}
+
+const App = () => {
+  const [ persons, setPersons ] = useState([
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
+  ])
+  const [ filter,  setFilter ] =  useState('')
+
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <Filter filter={filter} setFilter={setFilter} />
+      <h2>add a new</h2>
+      <PersonForm persons={persons} setPersons={setPersons}/>
       <h2>Numbers</h2>
-      {personsToShow().map(person => <p>{person.name} {person.number}</p>)}
+      <Persons persons={persons} filter={filter} />
     </div>
   )
 }
