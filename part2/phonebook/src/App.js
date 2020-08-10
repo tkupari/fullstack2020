1c4191e6c9db47dc09050eabf20eec7ee23bbb4a
Teemu Kupari <tkupari@gmail.com>
2020-08-10 16:32:38 +0300

    2.11: Fetch data from local json-server

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,4 +1,5 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 
 const Filter = ({ filter, setFilter }) => {
   const handleFilterChange = (event) => {
@@ -59,12 +60,15 @@ const Persons = ({ persons, filter }) => {
 
 const App = () => {
   const [ persons, setPersons ] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
   ])
   const [ filter,  setFilter ] =  useState('')
+
+  const loadData = () => {
+    axios.get('http://localhost:3001/persons').then(response =>
+      setPersons(response.data)
+    )
+  }
+  useEffect(loadData, [])
 
 
   return (
