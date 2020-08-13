6d724295af2ec8bafe47c8e1af6470450fcf4caf
Teemu Kupari <tkupari@gmail.com>
2020-08-13 10:12:19 +0300

    3.9: Use node backend for phonebook

--- part3/backend/index.js
+++ part3/backend/index.js
@@ -1,8 +1,10 @@
 const express = require('express')
+const cors = require('cors')
 const morgan = require('morgan')
 
 const app = express()
 app.use(express.json())
+app.use(cors())
 
 morganFormat = (tokens, req, res) => {
   return [
