5ab6669b5c12488c9fc97fe4d68a18ec2ab7aee6
Teemu Kupari <tkupari@gmail.com>
2020-08-13 09:04:41 +0300

    3.8: Adjust morgan logging format

--- part3/backend/index.js
+++ part3/backend/index.js
@@ -3,7 +3,19 @@ const morgan = require('morgan')
 
 const app = express()
 app.use(express.json())
-app.use(morgan('tiny'))
+
+morganFormat = (tokens, req, res) => {
+  return [
+    tokens.method(req, res),
+    tokens.url(req, res),
+    tokens.status(req, res),
+    tokens.res(req, res, 'content-length'), '-',
+    tokens['response-time'](req, res), 'ms',
+    JSON.stringify(req.body)
+  ].join(' ')
+}
+
+app.use(morgan(morganFormat))
 
 let persons = [
   {
