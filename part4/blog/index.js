8fc1750916725b9a7792a8ea59b5594123c0d9eb
Teemu Kupari <tkupari@gmail.com>
2020-08-17 14:08:34 +0300

    4.2: refactor model Blog into separate module

--- part4/blog/index.js
+++ part4/blog/index.js
@@ -5,15 +5,7 @@ const app = express()
 const cors = require('cors')
 const mongoose = require('mongoose')
 const logger = require('./utils/logger')
-
-const blogSchema = new mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
-})
-
-const Blog = mongoose.model('Blog', blogSchema)
+const Blog = require('./models/blog')
 
 const mongoUrl = process.env.MONGODB_URI
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
