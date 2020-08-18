2c48fe0b5f09a149803d74ec27f52b47cbdf14fb
Teemu Kupari <tkupari@gmail.com>
2020-08-18 09:32:25 +0300

    4.5: favoriteBlog helper

--- part4/blog/utils/list_helper.js
+++ part4/blog/utils/list_helper.js
@@ -4,6 +4,21 @@ const totalLikes = (blogs) => {
   return sum
 }
 
+const favoriteBlog = (blogs) => {
+  if(blogs.length === 0)
+    return null
+  const maxLikesReducer = (top, blog) => {
+    return top.likes > blog.likes ? top : blog
+  }
+  const topBlog = blogs.reduce(maxLikesReducer, blogs[0])
+  return {
+    title: topBlog.title,
+    author: topBlog.author,
+    likes: topBlog.likes
+  }
+}
+
 module.exports = {
-  totalLikes
+  totalLikes,
+  favoriteBlog,
 }
