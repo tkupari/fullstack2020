7b9e86ad7a3ea738581bd98d0a22beeab834ae62
Teemu Kupari <tkupari@gmail.com>
2020-08-19 15:57:46 +0300

    4.18: authentication token creation

--- part4/blog/app.js
+++ part4/blog/app.js
@@ -7,6 +7,7 @@ const config = require('./utils/config')
 const logger = require('./utils/logger')
 const blogsRouter = require('./controllers/blogs')
 const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
 const middleware = require('./utils/middleware')
 
 const mongoUrl = config.MONGODB_URI
@@ -24,6 +25,7 @@ app.use(middleware.requestLogger)
 
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
