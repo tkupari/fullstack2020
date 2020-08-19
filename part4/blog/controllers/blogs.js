8514ec3e48df1a8e349bbc4c7e7d2cfd5879c698
Teemu Kupari <tkupari@gmail.com>
2020-08-19 13:41:34 +0300

    4.17: add user to blog and blog to user blogs on blog creation

--- part4/blog/controllers/blogs.js
+++ part4/blog/controllers/blogs.js
@@ -1,19 +1,30 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
+const User = require('../models/user')
 
 blogsRouter.get('/', async (request, response) => {
-  const blogs = await Blog.find({})
+  const blogs = await Blog
+    .find({})
+    .populate('user', {username: 1, name: 1, id: 1})
   response.json(blogs)
 })
 
 blogsRouter.post('/', async (request, response) => {
   const blog = new Blog(request.body)
+  const user = await User.findOne({})
+  blog.user = user._id
   const result = await blog.save()
+  user.blogs = user.blogs.concat(blog._id)
+  await user.save()
+
+
   response.status(201).json(result)
 })
 
 blogsRouter.get('/:id', async (request, response) => {
-  const blog = await Blog.findById(request.params.id)
+  const blog = await Blog
+    .findById(request.params.id)
+    .populate('user', {username: 1, name: 1, id: 1})
   if(blog) {
     response.json(blog)
   } else {
