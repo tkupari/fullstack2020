ce0ea43c85a045ad181920583e3c01368057bc2e
Teemu Kupari <tkupari@gmail.com>
2020-08-24 15:26:02 +0300

    5.7: Add togglable displaying of details in Blog component

--- part5/bloglist-frontend/src/components/Blog.js
+++ part5/bloglist-frontend/src/components/Blog.js
@@ -1,8 +1,31 @@
-import React from 'react'
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
-)
+import React, { useState } from 'react'
+const Blog = ({ blog }) => {
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  const [showDetails, setShowDetails] = useState(false)
+
+  const toggleShowDetails = () => {
+    setShowDetails(!showDetails)
+  }
+  const displayStyle = { display: showDetails ? '' : 'none' }
+
+  return (
+    <div style={blogStyle}>
+      {blog.title} {blog.author}
+      <button onClick={toggleShowDetails}>{showDetails ? 'hide details' : 'show details'}</button>
+      <div style={displayStyle}>
+        {blog.url}<br/>
+        likes {blog.likes}<button>like</button><br/>
+        {blog.user.name}
+      </div>
+    </div>
+  )
+}
 
 export default Blog
