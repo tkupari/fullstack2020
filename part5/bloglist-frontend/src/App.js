dd2ca8f91c3f9e9346d1758945620fe6cc87acde
Teemu Kupari <tkupari@gmail.com>
2020-08-31 14:38:45 +0300

    Move BlogForm component into it's own file

--- part5/bloglist-frontend/src/App.js
+++ part5/bloglist-frontend/src/App.js
@@ -3,50 +3,7 @@ import Blog from './components/Blog'
 import blogService from './services/blogs'
 import loginService from './services/login'
 import Togglable from './components/togglable'
-
-const BlogForm = ({ createBlog }) => {
-  const [title, setTitle] = useState('')
-  const [author, setAuthor] = useState('')
-  const [url, setUrl] = useState('')
-
-  const handleBlogSumbit = (event) => {
-    event.preventDefault()
-    createBlog({ title, author, url })
-    setTitle('')
-    setUrl('')
-    setAuthor('')
-  }
-
-  return (
-    <div>
-      <h2>create new</h2>
-      <form onSubmit={handleBlogSumbit}>
-        <div>
-          title:
-          <input
-            value={title}
-            onChange={({ target }) => setTitle(target.value)}
-          />
-        </div>
-        <div>
-          author:
-          <input
-            value={author}
-            onChange={({ target }) => setAuthor(target.value)}
-          />
-        </div>
-        <div>
-          url:
-          <input
-            value={url}
-            onChange={({ target }) => setUrl(target.value)}
-          />
-        </div>
-        <button type="submit">create</button>
-      </form>
-    </div>
-  )
-}
+import BlogForm from './components/BlogForm'
 
 const Notification = ({ message, messageClass }) => {
   if(message === null)
---
+++ part5/bloglist-frontend/src/components/BlogForm.js
@@ -0,0 +1,47 @@
+import React, { useState } from 'react'
+
+const BlogForm = ({ createBlog }) => {
+  const [title, setTitle] = useState('')
+  const [author, setAuthor] = useState('')
+  const [url, setUrl] = useState('')
+
+  const handleBlogSumbit = (event) => {
+    event.preventDefault()
+    createBlog({ title, author, url })
+    setTitle('')
+    setUrl('')
+    setAuthor('')
+  }
+
+  return (
+    <div>
+      <h2>create new</h2>
+      <form onSubmit={handleBlogSumbit}>
+        <div>
+          title:
+          <input
+            value={title}
+            onChange={({ target }) => setTitle(target.value)}
+          />
+        </div>
+        <div>
+          author:
+          <input
+            value={author}
+            onChange={({ target }) => setAuthor(target.value)}
+          />
+        </div>
+        <div>
+          url:
+          <input
+            value={url}
+            onChange={({ target }) => setUrl(target.value)}
+          />
+        </div>
+        <button type="submit">create</button>
+      </form>
+    </div>
+  )
+}
+
+export default BlogForm
