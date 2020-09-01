f57fa2cc2d4c7e042a2ed6b4942b2822fd36c89d
Teemu Kupari <tkupari@gmail.com>
2020-09-01 14:42:48 +0300

    5.18: Login test

--- part5/bloglist-frontend/cypress/integration/blog_app.spec.js
+++ part5/bloglist-frontend/cypress/integration/blog_app.spec.js
@@ -1,10 +1,32 @@
 describe('Blog app', function() {
   beforeEach(function() {
     cy.request('POST', 'http://localhost:3001/api/testing/reset')
+    const user = {
+      name: 'Test User',
+      username: 'testuser',
+      password: 'sekret'
+    }
+    cy.request('POST', 'http://localhost:3001/api/users/', user)
     cy.visit('http://localhost:3000')
   })
 
   it('shows login form', function() {
     cy.get('#loginForm')
+  })
+
+  describe('login', function() {
+    it('succeeds with correct credentials', function() {
+      cy.get('#username').type('testuser')
+      cy.get('#password').type('sekret')
+      cy.get('#loginButton').click()
+      cy.contains('logged in as Test User')
+    })
+
+    it('fails with wrong credentials', function() {
+      cy.get('#username').type('testuser')
+      cy.get('#password').type('wrong')
+      cy.get('#loginButton').click()
+      cy.contains('Invalid username or password')
+    })
   })
 })
