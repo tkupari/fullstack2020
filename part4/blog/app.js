552a736e64238cfda693484ab88b1a8c8f39902e
Teemu Kupari <tkupari@gmail.com>
2020-09-01 14:02:52 +0300

    5.17: Add testingRouter to Blog app backend

--- part4/blog/app.js
+++ part4/blog/app.js
@@ -8,6 +8,7 @@ const logger = require('./utils/logger')
 const blogsRouter = require('./controllers/blogs')
 const usersRouter = require('./controllers/users')
 const loginRouter = require('./controllers/login')
+const testingRouter = require('./controllers/testingRouter')
 const middleware = require('./utils/middleware')
 
 const mongoUrl = config.MONGODB_URI
@@ -27,6 +28,10 @@ app.use(middleware.tokenExtractor)
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
 app.use('/api/login', loginRouter)
+
+if(process.env.NODE_ENV === 'test') {
+  app.use('/api/testing', testingRouter)
+}
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
