b95570c8407ee7880f292e40303c2e81fd276fc1
Teemu Kupari <tkupari@gmail.com>
2020-09-08 11:12:33 +0300

    6.15: initialize anecdotes with async action creator using redux-thunk

--- part6/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ part6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -1,3 +1,5 @@
+import anecdoteService from '../services/anecdotes'
+
 const sortByVotes = anecdotes => anecdotes.sort((a, b) => b.votes - a.votes)
 
 const reducer = (state = [], action) => {
@@ -35,10 +37,13 @@ export const createAnecdote = (anecdote) => {
   }
 }
 
-export const initializeAnecdotes = (anecdotes) => {
-  return {
-    type: 'INIT_ANECDOTES',
-    data: anecdotes
+export const initializeAnecdotes = () => {
+  return async dispatch => {
+    const anecdotes = await anecdoteService.getAll()
+    dispatch( {
+      type: 'INIT_ANECDOTES',
+      data: anecdotes
+    })
   }
 }
 
