887044eefe3abd4ad60043e7a8ba03d3b9f271a9
Teemu Kupari <tkupari@gmail.com>
2020-09-14 10:25:06 +0300

    7.2: Show single anecdote

--- part7/routed-anecdotes/src/App.js
+++ part7/routed-anecdotes/src/App.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 import {
-  BrowserRouter as Router,
-  Switch, Route, Link
+  Switch, Route, Link,
+  useRouteMatch
 } from 'react-router-dom'
 
 const Menu = () => {
@@ -21,8 +21,20 @@ const AnecdoteList = ({ anecdotes }) => (
   <div>
     <h2>Anecdotes</h2>
     <ul>
-      {anecdotes.map(anecdote => <li key={anecdote.id} >{anecdote.content}</li>)}
+      {anecdotes.map(anecdote =>
+        <li key={anecdote.id} >
+          <Link to={`/anecdotes/${anecdote.id}`}>{anecdote.content}</Link>
+        </li>
+      )}
     </ul>
+  </div>
+)
+
+const Anecdote = ({ anecdote }) => (
+  <div>
+    <h2>{anecdote.content}</h2>
+    <p>Has {anecdote.votes} votes</p>
+    <p>for more info see <Link to={anecdote.info}>{anecdote.info}</Link></p>
   </div>
 )
 
@@ -126,8 +138,10 @@ const App = () => {
     setAnecdotes(anecdotes.map(a => a.id === id ? voted : a))
   }
 
+  const match = useRouteMatch('/anecdotes/:id')
+  const anecdote = match ? anecdoteById(match.params.id) : null
+
   return (
-    <Router>
       <div>
         <h1>Software anecdotes</h1>
         <Menu />
@@ -138,13 +152,15 @@ const App = () => {
           <Route path='/create'>
             <CreateNew addNew={addNew} />
           </Route>
+          <Route path='/anecdotes/:id'>
+            <Anecdote anecdote={anecdote} />
+          </Route>`
           <Route path='/'>
             <AnecdoteList anecdotes={anecdotes} />
           </Route>
         </Switch>
         <Footer />
       </div>
-    </Router>
   )
 }
 
