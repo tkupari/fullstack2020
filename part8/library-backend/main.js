ba90fd949f7e47c9354d2c1c9bb5ae675aa0f1c1
Teemu Kupari <tkupari@gmail.com>
2020-09-24 12:59:45 +0300

    8.3: allAuthors query

--- part8/library-backend/main.js
+++ part8/library-backend/main.js
@@ -90,10 +90,15 @@ const typeDefs = gql`
     author: String!
     genres: [String!]!
   }
+  type Author {
+    name: String!
+    bookCount: Int!
+  }
   type Query {
     bookCount: Int!
     authorCount: Int!
     allBooks: [Book!]!
+    allAuthors: [Author!]!
   }
 `
 
@@ -101,7 +106,13 @@ const resolvers = {
   Query: {
     bookCount: () => books.length,
     authorCount: () => authors.length,
-    allBooks: () => books
+    allBooks: () => books,
+    allAuthors: () => authors.map(author => {
+      return {
+        name: author.name,
+        bookCount: books.filter(b => b.author == author.name).length
+      }
+    })
   }
 }
 
