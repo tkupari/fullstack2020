697b944a5e06d825ed9616e3df8186c115a75e6e
Teemu Kupari <tkupari@gmail.com>
2020-09-30 16:09:55 +0300

    9.5: WebBMI

--- part9/firststeps/bmiCalculator.ts
+++ part9/firststeps/bmiCalculator.ts
@@ -1,22 +1,4 @@
-const calculateBmi = (cm: number, kg: number): string => {
-  const m = cm / 100
-  const bmi = kg / (m * m)
-  if (bmi < 15)
-    return "Very severely underweight"
-  if (bmi < 16)
-    return "Severely underweight"
-  if (bmi < 18.5)
-    return "Underweight"
-  if (bmi < 25)
-    return "Normal (healthy weight)";
-  if (bmi < 30)
-    return "Overweight"
-  if (bmi < 35)
-    return "Obese Class I (Moderately obese)"
-  if (bmi < 40)
-    return "Obese Class II (Severely obese)"
-  return "Obese Class III (Very severely obese)"
-}
+import { calculateBmi } from './lib'
 
 const cm = Number(process.argv[2])
 const kg = Number(process.argv[3])
--- part9/firststeps/index.ts
+++ part9/firststeps/index.ts
@@ -1,11 +1,28 @@
 import express from 'express';
+import { calculateBmi } from './lib'
 
 const app = express();
 app.get('/hello', (_req, res) => {
   res.send('Hello Full Stack!');
 })
 
-const PORT = 3003;
+app.get('/bmi', (req, res) => {
+  const cm = Number(req.query.height)
+  const kg = Number(req.query.weight)
+  if(isNaN(cm) || isNaN(kg)) {
+    return res.json({
+      error: 'malformatted parameters'
+    })
+  }
+
+  return res.json({
+    weight: kg,
+    height: cm,
+    bmi: calculateBmi(cm, kg)
+  })
+})
+
+const PORT = 3002;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
